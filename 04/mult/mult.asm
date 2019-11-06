a6939d2ecb399b931b010311de87e1cd91c9549a
gotgotaind <gotgotaind@gmail.com>
2019-11-06 23:50:51 +0100

    Mult ok

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2
+M=0
+@i
+M=0
+
+(LOOP)
+	// Test i<R1
+	@i
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JGE
+	
+	// Add R0 + R2
+	@R0
+	D=M
+	@R2
+	M=D+M
+	
+	// Increment i
+	@i
+	M=M+1
+	
+	@LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
