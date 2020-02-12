1512cca116459827e02c4be76e016f56cf81bde7
gotgotaind <gotgotaind@gmail.com>
2020-02-12 23:12:48 +0100

    Adding skeleton files to project 10

---
+++ 10/jack_analyzer.py
@@ -0,0 +1,8 @@
+#input: a single .jack file or a directory containing containing
+# several jack files
+# for each file 
+#   create a jack_tokenizer from filename.jack
+#   create an output file names filename.xml
+#   and prepares it for writing
+#   creates and uses a compilation_engine to compile the jack_tokenizer
+#   into the output file
