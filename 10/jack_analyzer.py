be4f0bd241735313364462c10422bd66b84eae1e
gotgotaind <gotgotaind@gmail.com>
2020-02-24 22:17:13 +0100

    sutb for tokenizer

--- 10/jack_analyzer.py
+++ 10/jack_analyzer.py
@@ -6,3 +6,37 @@
 #   and prepares it for writing
 #   creates and uses a compilation_engine to compile the jack_tokenizer
 #   into the output file
+
+import sys
+import re
+import os
+import glob
+
+
+# if no argument is given set path as current directory
+if len(sys.argv) == 1:
+    print("No argument given, setting path to compile as current directory.")
+    path=os.path.abspath(".")
+else:
+    path=os.path.abspath(sys.argv[1])
+
+# if path is a directory, create a list of jack files to parse in the directory
+# else just parse the provided file
+if os.path.isdir(path):  
+    print("path is a directory.")
+    files_list=glob.glob(path+os.sep+"*.jack")
+    #opath=path+os.sep+os.path.basename(path)+'.asm'
+elif os.path.isfile(path):  
+    print("path is a file.")  
+    files_list=[path]
+    #[notext,ext]=os.path.splitext(path)
+    #opath=notext+'.asm'
+else:  
+    print("It is a special file (socket, FIFO, device file). Aborting..." )
+    exit(1)
+
+print(f"The following files will be compiled : {files_list}")
+
+
+
+
