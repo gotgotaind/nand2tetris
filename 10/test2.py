65f3a7e1042d025b6c56b44f6ad090a50a7448c7
gotgotaind <gotgotaind@gmail.com>
2020-03-09 23:33:21 +0100

    parsing comment, symbols, and identifiers ok

--- 10/test2.py
+++ 10/test2.py
@@ -14,11 +14,15 @@ def log(msg,char,line_num,char_in_line_num,char_num):
             char='\\n'
         print(f'{line_num}:{char_in_line_num} (char_num: {char_num}) (char is {char}), {msg}')
     
-def do_next_state(test_string,next_state,state,c,line_num,char_in_line_num,char_num):
-    if(c==test_string):
+def do_next_state(test_char,next_state,state,c,line_num,char_in_line_num,char_num):
+    if(c==test_char):
         state=next_state
         log(f'state is now {state}',c,line_num,char_in_line_num,char_num)
     return state
+    
+def out(token_type,token):
+    # example <keyword> class </keyword>
+    print(f'<{token_type}> {token} </{token_type}>')
     
 #initliazing the chars and lines positions counters
 char_num=1
@@ -27,24 +31,58 @@ char_in_line_num=1
 
 # we'll work with a state based parsing
 state='none'
+word=''
 
 for c in read_data:
     
-    if ( state == 'none' ):
-        state=do_next_state(test_string='/',next_state='/',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+    if ( state == 'identifier_or_keyword' ):
+        if ( c.isalpha() or c=='_' or c.isdigit() ):
+            word=word+c
+        else:
+            # end of the identifier_or_keyword
+            state='none'
+            out('identifier',word)
 
-        state=do_next_state(test_string='\'',next_state='StringConstant',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)       
-  
+
+    if ( state == 'none' ):
+        # a bare slash should be the start of a comment
+        state=do_next_state(test_char='/',next_state='/',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+
+        # a ' should be the start of a StringConstant
+        state=do_next_state(test_char='\'',next_state='StringConstant',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)       
+
+        # maybe we have a symbol?
+        if ( c in symbols ):
+            out('symbol',c)
+        
+        # maybe we have an identifier or a keyword
+        if ( c.isalpha() or c=='_' ):
+            state='identifier_or_keyword'
+            word=c
+            log(f'state is now {state}',c,line_num,char_in_line_num,char_num)
+
+
+
     elif ( state == '/' ):
-        state=do_next_state(test_string='/',next_state='//',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
-        state=do_next_state(test_string='*',next_state='/*',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+        state=do_next_state(test_char='/',next_state='//',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+        state=do_next_state(test_char='*',next_state='/*',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
         if ( state == '/' ):
             log(f'invalid syntax, was expecting a / or a *',c,line_num,char_in_line_num,char_num)
             exit(1)
 
     elif ( state == '//' ):
-        state=do_next_state(test_string='\n',next_state='none',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+        state=do_next_state(test_char='\n',next_state='none',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
             
+    elif ( state == '/*' ):
+        state=do_next_state(test_char='*',next_state='/*..*',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+
+    elif ( state == '/*..*' ):
+        state=do_next_state(test_char='/',next_state='none',state=state,c=c,line_num=line_num,char_in_line_num=char_in_line_num,char_num=char_num)
+        # if char is not /, go back to state '/*'
+        if ( state != 'none' ):
+            state = '/*'
+    
+      
         
     char_num=char_num+1
     if( c=='\n' ):
